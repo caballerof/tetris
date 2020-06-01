f494036f45462ba7a34e07f5b085b5c6ee02a36a
Reivaj Caballero <francisco.garcia@kueski.com>
2020-06-01 09:42:38 -0500

    Initial configuration project and project base code

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+
+  console.log(squares);
+  console.log(grid);
+});
